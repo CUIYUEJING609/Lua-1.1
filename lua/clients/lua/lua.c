ab8134030693ceb8a4a485731532892636557b48
Cui Yuejing <1740412032@qq.com>
2026-03-10 23:13:05 +0800

    refactor: replace gets with fgets (safer input)

--- lua/clients/lua/lua.c
+++ lua/clients/lua/lua.c
@@ -6,7 +6,7 @@
 char *rcs_lua="$Id: lua.c,v 1.1 1993/12/17 18:41:19 celes Exp $";
 
 #include <stdio.h>
-
+#include <string.h>
 #include "lua.h"
 #include "lualib.h"
 
@@ -19,8 +19,14 @@ void main (int argc, char *argv[])
  if (argc < 2)
  {
    char buffer[2048];
-   while (gets(buffer) != 0)
+      while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+   {
+     /* 去掉 fgets 读到的行尾换行符，避免影响解析 */
+     size_t len = strlen(buffer);
+     if (len > 0 && buffer[len-1] == '\n') buffer[len-1] = '\0';
+
      lua_dostring(buffer);
+   }
  }
  else
    for (i=1; i<argc; i++)
